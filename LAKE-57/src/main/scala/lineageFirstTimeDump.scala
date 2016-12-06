bd2438e9afcdb618fd271226d3b02ca45a744516
SHVinuta <vinuta.hegde@antuit.com>
2016-12-06 18:40:13 +0530

    first time dump temp

---
+++ LAKE-57/src/main/scala/lineageFirstTimeDump.scala
@@ -0,0 +1,62 @@
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import java.security.MessageDigest
+import org.apache.spark.sql.Dataset
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import scala.reflect.runtime.universe
+import org.apache.spark.SparkContext
+object lineageFirstTimeDump {
+
+	def addHash(deltaDf: Dataset[Row]): Dataset[Row] = {
+			    val sparkSession = deltaDf.sparkSession
+					sparkSession.udf.register("sha2m", (r: Row) => {
+						val sha2Hasher = MessageDigest.getInstance("SHA-256")
+								val buf = new StringBuilder
+								val bytes = r.mkString(",").getBytes
+								val hexString: StringBuffer = new StringBuffer
+								sha2Hasher.digest(bytes).foreach { b => hexString.append(Integer.toHexString(0xFF & b))
+								}
+								hexString.toString()
+					})
+
+					deltaDf.createOrReplaceTempView("delta_table")
+
+					sparkSession.sql("select *, sha2m(struct(*)) as sha2 from delta_table")
+	}
+
+	def addDeltaFirstTime(initialDf: Dataset[Row], deltaDf: Dataset[Row]): Dataset[Row] = {
+			    val sparkSession = deltaDf.sparkSession
+					val initialDfSha = addHash(initialDf.drop("archive_date"))
+					val deltaDfSha = addHash(deltaDf)
+
+					val deduped = initialDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.
+					reduceByKey((r1, r2) => r1).
+					map { case(sha2, row) => row }
+
+					val dedupedDf = sparkSession.createDataFrame(deduped, deltaDfSha.schema) 
+					dedupedDf.createOrReplaceTempView("deduped")
+					import org.apache.spark.sql.functions._ 
+					dedupedDf.withColumn("sequence", monotonically_increasing_id)
+	}
+
+	
+def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("test").setMaster("local")
+	val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
+    val df1 = sqlContext.sql("select * from antuit_stage.data1")
+    val df2 = sqlContext.sql("select * from antuit_stage.data2")
+    val df3 = addDeltaFirstTime(df1, df2)
+	df3.show()
+	val df4 = sqlContext.sql("select * from antuit_stage.data3")
+    val finalRes = addDeltaIncremental(df3, df4)
+	finalRes.show()
+    }
+
+}
+   
+
+}
