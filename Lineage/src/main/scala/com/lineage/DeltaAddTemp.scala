e446711bdf11a0d72087ce45698284d20c11c8d3
SHVinuta <vinuta.hegde@antuit.com>
2016-12-13 14:41:24 +0530

    13_12

--- Lineage/src/main/scala/com/lineage/DeltaAddTemp.scala
+++ Lineage/src/main/scala/com/lineage/DeltaAddTemp.scala
@@ -43,11 +43,11 @@ object DeltaAddTemp {
         val Month1 =cal.get(Calendar.MONTH )
         val Month = Month1+1
 
-        res.write.format("com.databricks.spark.csv").option("delimiter", "\u0001").save("/antuit/databases/antuit_stage/"+args(0)+"/"+Date+"_"+Month)
+        res.write.format("com.databricks.spark.csv").option("delimiter", "\u0001").save("/antuit/databases/antuit_stage/testTable/"+Date+"_"+Month)
         sqlContext.sql("create table antuit_stage."+args(0)+"_merge like antuit_stage."+ args(0))
         sqlContext.sql("drop table if exists antuit_stage."+args(0))
         sqlContext.sql("create table antuit_stage."+args(0)+" like antuit_stage."+ args(0)+"_merge")
-        sqlContext.sql("ALTER TABLE "+ args(0) +" set location \'/antuit/databases/antuit_stage/"+args(0)+"/"+Date+"_"+Month+"\'")
-        sqlContext.sql("drop table if exists antuit_stage."+args(0)+"_merge")
+        sqlContext.sql("ALTER TABLE "+ args(0) +" set location \'/antuit/databases/antuit_stage/testTable/"+Date+"_"+Month+"\'")
+        //sqlContext.sql("drop table if exists antuit_stage."+args(0)+"_merge")
       }
 }
