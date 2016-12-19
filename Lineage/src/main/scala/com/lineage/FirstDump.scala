972bbfcea1a27049ed48788b784b20fb39dd472c
SHVinuta <vinuta.hegde@antuit.com>
2016-12-19 17:06:49 +0530

    19_12

--- Lineage/src/main/scala/com/lineage/FirstDump.scala
+++ Lineage/src/main/scala/com/lineage/FirstDump.scala
@@ -20,7 +20,7 @@ object FirstDump {
 			    val sparkSession = deltaDf.sparkSession
        if(!(deltaDf.columns.contains("archive_date")))
          {
-         deltaDf.withColumn("archive_date")
+         deltaDf.withColumn("archive_date","null")
          }
 			    
 			   // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
