66b691d705c76b639e10ee7925735c25a1ba61f7
SHVinuta <vinuta.hegde@antuit.com>
2016-12-27 15:41:44 +0530

    27_12

--- Lineage/src/main/scala/com/lineage/FirstDump.scala
+++ Lineage/src/main/scala/com/lineage/FirstDump.scala
@@ -19,36 +19,11 @@ object FirstDump {
  * */
 	def addDeltaFirstTime(initialDf: Dataset[Row], deltaDf: Dataset[Row]): Dataset[Row] = {
 			    val sparkSession = deltaDf.sparkSession
-       if(!(deltaDf.columns.contains("archive_date")))
-         {
-         
-         
-        
-       // val delta =  deltaDf.withColumn("archive_date",lit("delta"))
-        //delta.
-			   // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
-			    val sortedinitialDf = initialDf.drop("archive_date")//select(deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
-			    val sortedDelta =     deltaDf//.select(deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
-			    
-          val initialDfSha = RowHash.addHash(sortedinitialDf)//.drop("archive_date"))
-					val deltaDfSha = RowHash.addHash(sortedDelta)//.drop("archive_date"))
-					////initialDfSha.show()
-					//deltaDfSha.show()
-					val deduped = initialDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).	map { case(sha2, row) => row }
-					
-					val dedupedDf = sparkSession.createDataFrame(deduped, deltaDfSha.schema) 
-					dedupedDf.show()
-					dedupedDf.createOrReplaceTempView("deduped")
-					import org.apache.spark.sql.functions._ 
-					dedupedDf.withColumn("sequence", monotonically_increasing_id) 
-         }
-       else
-       {
-         val delta =  deltaDf
-         delta.show()
-			   // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
-			    val sortedinitialDf = initialDf.select("archive_date" , delta.columns.filter(x => !x.equals("archive_date")):_*)
-			    val sortedDelta =     delta.select("archive_date" , delta.columns.filter(x => !x.equals("archive_date")):_*)
+            
+             
+          // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
+			    val sortedinitialDf = initialDf.select("archive_date" , deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
+			    val sortedDelta =     deltaDf.select("archive_date" , deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
           val initialDfSha = RowHash.addHash(sortedinitialDf)//.drop("archive_date"))
 					val deltaDfSha = RowHash.addHash(sortedDelta)//.drop("archive_date"))
 					val deduped = initialDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).	map { case(sha2, row) => row }
@@ -56,7 +31,7 @@ object FirstDump {
 					dedupedDf.createOrReplaceTempView("deduped")
 					import org.apache.spark.sql.functions._ 
 					dedupedDf.withColumn("sequence", monotonically_increasing_id) 
-       }
+    
 			    
 	}
 		
@@ -66,13 +41,17 @@ object FirstDump {
       val sc = new SparkContext(conf)
       val sqlContext = new org.apache.spark.sql.SQLContext(sc)
       import sqlContext.implicits._
-      val archData = sqlContext.sql("select * from staging."+args(0)) // Load archive data
+      if(args(2).equals(null))
+          {
+         args(2) = args(1)
+          }
+      val archData = sqlContext.sql("select * from staging."+args(2)) // Load archive data
       val LatestData = sqlContext.sql("select * from sqoopdailydelta."+args(1)) // Load latest data from impala
       val res = addDeltaFirstTime(archData, LatestData)
       //res.show()
       res.registerTempTable("mytempTable")
-      sqlContext.sql("drop table if exists antuit_stage."+args(2))
-      sqlContext.sql("create table antuit_stage."+args(2)+" as select * from mytempTable");
+      sqlContext.sql("drop table if exists antuit_stage."+args(0))
+      sqlContext.sql("create table antuit_stage."+args(0)+" as select * from mytempTable");
   
   
       }
