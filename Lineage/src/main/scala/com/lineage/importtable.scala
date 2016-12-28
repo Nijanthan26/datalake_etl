761644424508eac70f2978ce01c720bd0f01940b
SHVinuta <vinuta.hegde@antuit.com>
2016-12-28 19:38:02 +0530

    28_12

--- Lineage/src/main/scala/com/lineage/importtable.scala
+++ Lineage/src/main/scala/com/lineage/importtable.scala
@@ -17,10 +17,15 @@ object importtable {
 
 val dataframe_db = sqlcontext.read.format("jdbc").
 option("url", "jdbc:sqlserver://192.168.100.223:1433/database=AAD").
-option("driver", "com.mysql.jdbc.Driver").
+option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver").
 option("dbtable", "t_bmm_customer").
 option("user", "readonly").
 option("password", "HJ#ric1!").load()
+
+
+
+
+
 dataframe_db.rdd.map { x => x.mkString("\u0001")}.saveAsTextFile("/antuit_stage/hj_bmm_event_log")
 
 
