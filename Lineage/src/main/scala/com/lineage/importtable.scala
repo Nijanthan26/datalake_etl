6e5e2184a3f0eab9f4b00e34827fc061100bd07c
SHVinuta <vinuta.hegde@antuit.com>
2016-12-28 17:13:06 +0530

    28_12

--- Lineage/src/main/scala/com/lineage/importtable.scala
+++ Lineage/src/main/scala/com/lineage/importtable.scala
@@ -3,6 +3,9 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql._
+import org.apache.spark.sql.SQLContext
+
+
 
 object importtable {
   
@@ -10,8 +13,9 @@ object importtable {
    val  tablename= args(0)
    val conf = new SparkConf().setAppName("Load Data from DB")
    val sc = new SparkContext(conf)
+   val sqlcontext = new org.apache.spark.sql.SQLContext(sc)
 
-val dataframe_db = sc.read.format("jdbc").option("url", "jdbc:sqlserver://192.168.100.223:1433;database=AAD").option("dbtable", "t_bmm_event_log").option("user", "readonly").option("password", "HJ#ric1!").load()
+val dataframe_db = sqlcontext.read.format("jdbc").option("url", "jdbc:sqlserver://192.168.100.223:1433;database=AAD").option("dbtable", "t_bmm_event_log").option("user", "readonly").option("password", "HJ#ric1!").load()
 dataframe_db.rdd.map { x => x.mkString("\u0001")}.saveAsTextFile("/antuit_stage/hj_bmm_event_log")
 
 
