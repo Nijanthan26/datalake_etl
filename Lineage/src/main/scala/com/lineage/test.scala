57bb7e53da3c7a1bf081561fc00578a03e689100
SHVinuta <vinuta.hegde@antuit.com>
2017-01-20 20:25:14 +0530

    sql run test

--- Lineage/src/main/scala/com/lineage/test.scala
+++ Lineage/src/main/scala/com/lineage/test.scala
@@ -13,43 +13,14 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.functions._
 
 object test {
-/*
- * To add md5 and sequence numbers to archive data and the latest data present in impala and dump the combined data in data lake.
- * This is one time run. 
- * */
-	def addDeltaFirstTime(initialDf: Dataset[Row], deltaDf: Dataset[Row]): Dataset[Row] = {
-			    val sparkSession = deltaDf.sparkSession
-
-         val delta =  deltaDf
-         delta.show()
-			   // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
-			    //val sortedinitialDf = initialDf.select("archive_date" , delta.columns.filter(x => !x.equals("archive_date")):_*)
-			    //val sortedDelta =     delta.select("archive_date" , delta.columns.filter(x => !x.equals("archive_date")):_*)
-          val initialDfSha = RowHash.addHash(initialDf)//.drop("archive_date"))
-					val deltaDfSha = RowHash.addHash(deltaDf)//.drop("archive_date"))
-					val deduped = initialDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).	map { case(sha2, row) => row }
-					val dedupedDf = sparkSession.createDataFrame(deduped, deltaDfSha.schema) 
-					dedupedDf.createOrReplaceTempView("deduped")
-					import org.apache.spark.sql.functions._ 
-					dedupedDf.withColumn("sequence", monotonically_increasing_id) 
-       
-			    
-	}
-		
   
   def main(args: Array[String]): Unit = {
-      val conf = new SparkConf().setAppName("FirstDump")
+      val conf = new SparkConf().setAppName("sqlrun")
       val sc = new SparkContext(conf)
       val sqlContext = new org.apache.spark.sql.SQLContext(sc)
       import sqlContext.implicits._
-      val archData = sqlContext.sql("select * from sqoopdailydelta."+args(0)) // Load archive data
-      val LatestData = sqlContext.sql("select * from sqoopdailydelta."+args(1)) // Load latest data from impala
-      val res = addDeltaFirstTime(archData, LatestData)
-      res.registerTempTable("mytempTable")
-      sqlContext.sql("drop table if exists antuit_stage."+args(2))
-      sqlContext.sql("create table antuit_stage."+args(2)+" as select * from mytempTable");
-  
-  
+      val archData = sqlContext.sql(args(0)) // Load archive data
+       
       }
   
 }
