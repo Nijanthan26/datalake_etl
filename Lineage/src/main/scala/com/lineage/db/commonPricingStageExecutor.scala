1fe94718eb3e866d9d0e863b12e0847df5c9fe40
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-02 11:38:40 +0530

    Feb 2nd,1st commit

--- Lineage/src/main/scala/com/lineage/db/commonPricingStageExecutor.scala
+++ Lineage/src/main/scala/com/lineage/db/commonPricingStageExecutor.scala
@@ -31,62 +31,74 @@ object commonPricingStageExecutor {
 					prop.setProperty("user",username)
 					prop.setProperty("password",password)
   
-					val mrsStagingDf = sqlContext.sql("""select distinct nvl((case when a.facilityid like '0%' then cast(cast(a.facilityid as int) as string) else cast(a.facilityid as string) end),cast('' as string)) facilityid 
-,nvl((case when a.fcustcode like '0%' then cast(cast(a.fcustcode as int) as string) else cast(a.fcustcode as string) end),cast('NA' as string)) fcustcode 
-,c.flot 
-,d.finvoice 
-,a.fdatestamp 
-,d.forigamt 
-,d.frate 
-,d.fqty as fqty_billed 
-,d.fweight as fweight_billed 
-,d.fbasis 
-,d.fgl  
-,d.fnumdays
-,d.fbilltype
-,d.fnotes
-,(case when upper(d.fbilltype)='RS' then e.frs_days when upper(d.fbilltype)='IS' then e.fis_days else d.fnumdays end) as ren_pd
-,d.fbilledby from antuit_stage.dl_mrs_phy_trn_delta a left join antuit_stage.dl_mrs_phy_mst_delta pm on (concat(a.fbatch
-,a.fsequence) = pm.ftrack and
-a.fserial = pm.fserial and
-a.fcustcode = pm.fcustcode and
-a.facilityid = pm.facilityid
-)
-left join antuit_stage.dl_mrs_inv_trn_delta b on
-(a.facilityid = b.facilityid and
-a.fcustcode = b.fcustcode and 
-concat(a.fbatch
-,a.fsequence) = concat(b.fbatch
-,b.fsequence) and
-a.frectype = b.frectype and a.ftrack = b.ftrack) left join antuit_stage.dl_mrs_inv_mst_delta c on
-(b.facilityid = c.facilityid and
-b.fcustcode = c.fcustcode and
-concat(b.fbatch
-,b.fsequence) = concat(c.fbatch
-,c.fsequence) and
-b.frectype = c.frectype and
-b.fprodgroup = c.fprodgroup and
-b.fproduct = c.fproduct)  
-left join antuit_stage.dl_mrs_billhist_delta d on 
-(c.facilityid = d.facilityid and
-c.fcustcode = d.fcustcode and 
-concat(c.fbatch
-,c.fsequence) = concat(d.fbatch
-,d.fsequence) and 
-c.flot = d.flot and
-c.fprodgroup = d.fprodgroup and 
-c.fproduct = d.fproduct and
-c.fpricecode = d.fpricecode
-) left join antuit_stage.dl_mrs_price_delta e on (
-c.fcustcode = e.fcustcode and 
-c.facilityid = e.facilityid and
-c.fpricecode = e.fpricecode
-)""")
+					val mrsStagingDf1 = sqlContext.sql("""select  
+nvl((case when matc.facilityid like '0%' then cast(cast(matc.facilityid as int) as string) else cast(matc.facilityid as string) end),cast('' as string)) as facilityid 
+,nvl((case when matc.fcustcode like '0%' then cast(cast(matc.fcustcode as int) as string) else cast(matc.fcustcode as string) end),cast('' as string)) as fcustcode 
+,matc.flot 
+,matc.finvoice 
+,matc.fidate 
+,matc.forigamt 
+,matc.frate 
+,matc.fqty as fqty_billed 
+,matc.fweight as fweight_billed 
+,matc.fbasis 
+,matc.fgl  
+,matc.fnumdays
+,matc.fbilltype
+,matc.fnotes
+,(case when upper(matc.fbilltype)='RS' then e.frs_days
+when upper(matc.fbilltype)='IS' then e.fis_days else matc.fnumdays end) as ren_pd
+,matc.fbilledby 
+from 
+(select distinct d.*,c.facilityid as facilityid2,c.ftrack2,c.fcustcode as fcustcode2
+from antuit_stage.dl_mrs_billhist_delta d
+inner join (select distinct facilityid,ftrack2,fcustcode from staging.mrs_inv_mst_delta) c
+on 
+(d.facilityid=c.facilityid
+and d.fcustcode=c.fcustcode
+and d.ftrack=c.ftrack2)) matc
+left join antuit_stage.dl_mrs_price_delta e on (
+matc.fcustcode = e.fcustcode and 
+matc.facilityid = e.facilityid and
+matc.fpricecode = e.fpricecode)""")
+
+					val mrsStagingDf2 = sqlContext.sql("""select 
+nvl((case when matc.facilityid like '0%' then cast(cast(matc.facilityid as int) as string) else cast(matc.facilityid as string) end),cast('' as string)) as facilityid 
+,nvl((case when matc.fcustcode like '0%' then cast(cast(matc.fcustcode as int) as string) else cast(matc.fcustcode as string) end),cast('' as string)) as fcustcode 
+,matc.flot 
+,matc.finvoice 
+,matc.fidate 
+,matc.forigamt 
+,matc.frate 
+,matc.fqty as fqty_billed 
+,matc.fweight as fweight_billed 
+,matc.fbasis 
+,matc.fgl  
+,matc.fnumdays
+,matc.fbilltype
+,matc.fnotes
+,(case when upper(matc.fbilltype)='RS' then e.frs_days
+when upper(matc.fbilltype)='IS' then e.fis_days else matc.fnumdays end) as ren_pd
+,matc.fbilledby 
+from 
+(select distinct d.*,c.facilityid as facilityid2,c.ftrack2,c.fcustcode as fcustcode2
+from antuit_stage.dl_mrs_billhist_delta d
+left outer join (select distinct facilityid,ftrack2,fcustcode from staging.mrs_inv_mst_delta) c
+on 
+(d.facilityid=c.facilityid
+and d.fcustcode=c.fcustcode
+and d.ftrack=c.ftrack2)
+where c.ftrack2 is null
+) matc
+left join antuit_stage.dl_mrs_price_delta e on (
+matc.fcustcode = e.fcustcode and 
+matc.facilityid = e.facilityid and
+matc.fpricecode = e.fpricecode)""")
 
 
+val mrsStagingDf = mrsStagingDf1.unionAll(mrsStagingDf2)
 
-
-val hjPreStaging1Df = sqlContext.sql("""select 
+val hjPreStaging1Df = sqlContext.sql("""select distinct
 d.wh_id,
 m.customer_code,
 d.lot_number,
@@ -102,18 +114,20 @@ cast(null as string) as hj_ren_pd,
 d.uom,
 g.chargeback_type
 from
-antuit_stage.hj_t_bmm_charge_event_ref c
-inner join antuit_stage.hj_t_bmm_charge d on (c.charge_id = d.charge_id)
-inner join antuit_stage.hj_t_bmm_chargeback_rate chrate on (d.chargeback_rate_id = chrate.chargeback_rate_id)	
-inner join antuit_stage.hj_t_bmm_cont_inv_type_chargeback f on (d.chargeback_id = f.chargeback_id and chrate.chargeback_id = f.chargeback_id) 
-inner join antuit_stage.hj_t_bmm_invoice inv on (d.invoice_id=inv.invoice_id)
-inner join antuit_stage.hj_t_bmm_contract_invoice_type k on (inv.contract_invoice_type_id = k.contract_invoice_type_id and f.contract_invoice_type_id=k.contract_invoice_type_id) 
-inner join antuit_stage.hj_t_bmm_contract_master l on (k.contract_id = l.contract_id) 
-inner join antuit_stage.hj_t_bmm_customer m on (l.customer_id = m.customer_id)	
-inner join (select distinct chargeback_type_id,chargeback_type from antuit_stage.hj_t_bmm_chargeback_type where chargeback_type='WMS Event') g on (f.chargeback_type_id = g.chargeback_type_id)""")
+(select distinct charge_id from antuit_stage.hj_t_bmm_charge_event_ref) c
+left join antuit_stage.hj_t_bmm_charge d on (c.charge_id = d.charge_id)
+left join antuit_stage.hj_t_bmm_chargeback_rate chrate on (d.chargeback_rate_id = chrate.chargeback_rate_id)	
+left join (select chargeback_id,contract_invoice_type_id,chargeback_type_id,chargeback_code from antuit_stage.hj_t_bmm_cont_inv_type_chargeback) f on (d.chargeback_id = f.chargeback_id and chrate.chargeback_id = f.chargeback_id) 
+left join (select invoice_id,contract_invoice_type_id,generated_date from antuit_stage.hj_t_bmm_invoice) inv on (d.invoice_id=inv.invoice_id)
+left join antuit_stage.hj_t_bmm_contract_invoice_type k on (inv.contract_invoice_type_id = k.contract_invoice_type_id and f.contract_invoice_type_id=k.contract_invoice_type_id) 
+left join (select contract_id,customer_id from antuit_stage.hj_t_bmm_contract_master) l on (k.contract_id = l.contract_id) 
+left join (select customer_code,customer_id from antuit_stage.hj_t_bmm_customer) m on (l.customer_id = m.customer_id)	
+left join (select distinct chargeback_type_id,chargeback_type from antuit_stage.hj_t_bmm_chargeback_type where chargeback_type='WMS Event') g on (f.chargeback_type_id = g.chargeback_type_id)""")
 
 hjPreStaging1Df.registerTempTable("hj_pre_staging_1")
+
 mrsStagingDf.registerTempTable("mrs_staging")
+
 val hjStagingDf1 = sqlContext.sql("select distinct * from hj_pre_staging_1")
 
 
@@ -134,13 +148,13 @@ chg.wh_id
 ,ct.chargeback_type
 from
 antuit_stage.hj_t_bmm_charge chg 
-inner join antuit_stage.hj_t_bmm_cont_inv_type_chargeback citc on (chg.chargeback_id = citc.chargeback_id)
-inner join antuit_stage.hj_t_bmm_chargeback_type ct on (citc.chargeback_type_id = ct.chargeback_type_id)
-inner join antuit_stage.hj_t_bmm_chargeback_rate cbrate on (citc.chargeback_id = cbrate.chargeback_id and cbrate.chargeback_rate_id=chg.chargeback_rate_id)
-inner join antuit_stage.hj_t_bmm_invoice inv on (chg.invoice_id = inv.invoice_id)
-inner join antuit_stage.hj_t_bmm_contract_invoice_type cit on (cit.contract_invoice_type_id = inv.contract_invoice_type_id and citc.contract_invoice_type_id=cit.contract_invoice_type_id)
-inner join antuit_stage.hj_t_bmm_contract_master cm on (cit.contract_id = cm.contract_id)
-inner join antuit_stage.hj_t_bmm_customer cust on (cm.customer_id = cust.customer_id)
+LEFT join antuit_stage.hj_t_bmm_cont_inv_type_chargeback citc on (chg.chargeback_id = citc.chargeback_id)
+left join antuit_stage.hj_t_bmm_chargeback_type ct on (citc.chargeback_type_id = ct.chargeback_type_id)
+left join antuit_stage.hj_t_bmm_chargeback_rate cbrate on (citc.chargeback_id = cbrate.chargeback_id and cbrate.chargeback_rate_id=chg.chargeback_rate_id)
+left join antuit_stage.hj_t_bmm_invoice inv on (chg.invoice_id = inv.invoice_id)
+left join antuit_stage.hj_t_bmm_contract_invoice_type cit on (cit.contract_invoice_type_id = inv.contract_invoice_type_id and citc.contract_invoice_type_id=cit.contract_invoice_type_id)
+left join antuit_stage.hj_t_bmm_contract_master cm on (cit.contract_id = cm.contract_id)
+left join antuit_stage.hj_t_bmm_customer cust on (cm.customer_id = cust.customer_id)
 left join antuit_stage.hj_t_bmm_param_uom_storage pus on (citc.chargeback_id = pus.chargeback_id)
 left join antuit_stage.hj_t_bmm_frequency freq on (citc.chargeback_id = freq.chargeback_id)
 left join antuit_stage.hj_t_bmm_param_uom_anniversary pua on (citc.chargeback_id = pua.chargeback_id)
@@ -148,7 +162,7 @@ left join antuit_stage.hj_t_bmm_param_recurring pr on (citc.chargeback_id = pr.c
 left join antuit_stage.hj_t_bmm_param_manual_csr_prompt mcp on (citc.chargeback_id = mcp.chargeback_id)
 left join antuit_stage.hj_t_bmm_param_manual_prompt mp on (citc.chargeback_id = mp.chargeback_id)
 left join antuit_stage.hj_t_bmm_param_manual_adhoc ma on (citc.chargeback_id = ma.chargeback_id)
-where lower(ct.chargeback_type) <>'wms event' """)
+where lower(ct.chargeback_type) <>'wms event'""")
 
 hjPreStaging2Df.registerTempTable("hj_pre_staging_2")
 
@@ -159,8 +173,9 @@ val hjStagingDf = hjStagingDf1.unionAll(hjPreStaging2Df)
 hjStagingDf.registerTempTable("hj_staging")
 
 
-val mrsCustXrefDf = sqlContext.sql("""select distinct
+val mrsCustXrefDf = sqlContext.sql("""select
 tab.*
+,acxref.currencyisocode
 ,acxref.NAME
 ,acxref.BILLINGSTREET
 ,acxref.BILLINGCITY
@@ -172,10 +187,9 @@ tab.*
 ,acxref.ACCOUNTNUMBER
 ,acxref.WEBSITE 
 from
-(select distinct
+(select 
 stg.*
 ,concat('MRS',stg.facilityid, nvl(stg.fcustcode,cast('' as string))) as UID_stg
---,xref.UID
 ,'MRS' as LIN_SOURCE_SYSTEM_NAME__C
 ,xref.LIN_CUSTOMER_ENTERPRISE_ID__C
 ,xref.LIN_SURVIVOR_CUSTOMER_NAME__C
@@ -185,13 +199,13 @@ stg.*
 ,costc.LIN_WORKDAY_COST_CENTER__C
 ,costc.LIN_WORKDAY_LOCATION_ID__C
 from 
-mrs_staging stg
+antuit_pricing.mrs_staging stg
 left join (select distinct uid,LIN_ACCOUNT__C,LIN_CUSTOMER_ENTERPRISE_ID__C,LIN_SURVIVOR_CUSTOMER_NAME__C,LIN_SOURCE_SYSTEM_NAME__C from antuit_pricing.customer_xref where LIN_SOURCE_SYSTEM_NAME__C ='MRS') xref
 on concat('MRS', nvl((case when stg.facilityid like '0%' then cast(cast(stg.facilityid as int) as string) else cast(stg.facilityid as string) end),cast('' as string)),
 nvl((case when stg.fcustcode like '0%' then cast(cast(stg.fcustcode as int) as string) else cast(stg.fcustcode as string) end),cast('' as string)))= xref.UID
-left join (select distinct uid,LIN_CONSOLIDATED_CHARGE_CODE__C,LIN_CONSOLIDATED_CHARGE_NAME__C from antuit_pricing.chargecode_xref) chxref
+left join (select distinct uid,LIN_CONSOLIDATED_CHARGE_CODE__C,LIN_CONSOLIDATED_CHARGE_NAME__C from antuit_pricing.chargecode_xref where lin_legacy_system__c='MRS') chxref
 on concat('MRS',nvl((case when stg.fgl like '0%' then cast(cast(stg.fgl as int) as string) else cast(stg.fgl as string) end),cast('' as string)))=chxref.uid
-left join (select distinct uid,LIN_WORKDAY_COST_CENTER__C,LIN_WORKDAY_LOCATION_ID__C from antuit_pricing.costcenter_xref) costc
+left join (select distinct uid,LIN_WORKDAY_COST_CENTER__C,LIN_WORKDAY_LOCATION_ID__C from antuit_pricing.costcenter_xref where legacy_lin_system__c='MRS') costc
 on concat('MRS',nvl((case when stg.facilityid like '0%' then cast(cast(stg.facilityid as int) as string) else cast(stg.facilityid as string) end),cast('' as string)))=costc.uid
 ) tab
 LEFT JOIN antuit_pricing.account_revised_xref ACXREF
@@ -199,8 +213,9 @@ ON (tab.LIN_ACCOUNT__C=acxref.id)""")
 
 mrsCustXrefDf.registerTempTable("mrs_cust_xref")
 
-val hjCustRefDf = sqlContext.sql("""select distinct
+val hjCustRefDf = sqlContext.sql("""select 
 tab.*
+,acxref.currencyisocode
 ,acxref.NAME
 ,acxref.BILLINGSTREET
 ,acxref.BILLINGCITY
@@ -213,7 +228,7 @@ tab.*
 ,acxref.WEBSITE 
 from
 (
-select distinct
+select 
 stg.*
 ,concat('HIGHJUMP', nvl(stg.customer_code,cast('' as string))) as UID_stg
 ,cast('HIGHJUMP' as string) as LIN_SOURCE_SYSTEM_NAME__C
@@ -225,12 +240,12 @@ stg.*
 ,costc.LIN_WORKDAY_COST_CENTER__C
 ,costc.LIN_WORKDAY_LOCATION_ID__C
 from 
-hj_staging stg	
+antuit_pricing.HJ_staging stg	
 left join (select distinct LIN_ACCOUNT__C,LIN_CUSTOMER_ENTERPRISE_ID__C,LIN_SURVIVOR_CUSTOMER_NAME__C,LIN_SOURCE_SYSTEM_NAME__C,LIN_LEGACY_CUSTOMER_CODE__C from antuit_pricing.customer_xref where LIN_SOURCE_SYSTEM_NAME__C ='HIGHJUMP') xref
 on (concat('HIGHJUMP', nvl(stg.customer_code,cast('' as string))))=(concat(nvl(xref.LIN_SOURCE_SYSTEM_NAME__C,cast('' as string)),nvl(xref.LIN_LEGACY_CUSTOMER_CODE__C,cast('' as string))))
-left join (select distinct uid,LIN_CONSOLIDATED_CHARGE_CODE__C,LIN_CONSOLIDATED_CHARGE_NAME__C from antuit_pricing.chargecode_xref) chxref
+left join (select distinct uid,LIN_CONSOLIDATED_CHARGE_CODE__C,LIN_CONSOLIDATED_CHARGE_NAME__C,lin_legacy_charge_code__c from antuit_pricing.chargecode_xref where lin_legacy_system__c='HIGHJUMP') chxref
 on concat('HIGHJUMP',nvl((case when stg.chargeback_code like '0%' then cast(cast(stg.chargeback_code as int) as string) else cast(stg.chargeback_code as string) end),cast('' as string)))=chxref.uid
-left join (select distinct uid,LIN_WORKDAY_COST_CENTER__C,LIN_WORKDAY_LOCATION_ID__C from antuit_pricing.costcenter_xref) costc
+left join (select distinct uid,LIN_WORKDAY_COST_CENTER__C,LIN_WORKDAY_LOCATION_ID__C from antuit_pricing.costcenter_xref where legacy_lin_system__c='HIGHJUMP') costc
 on concat('HIGHJUMP',nvl((case when stg.wh_id like '0%' then cast(cast(stg.wh_id as int) as string) else cast(stg.wh_id as string) end),cast('' as string)))=costc.uid
 ) tab
 LEFT JOIN antuit_pricing.account_revised_xref ACXREF
@@ -239,7 +254,7 @@ ON (tab.LIN_ACCOUNT__C=acxref.id)""")
 hjCustRefDf.registerTempTable("hj_cust_xref")
 
 
-val commonPricingStageDf1 = sqlContext.sql("""select distinct
+val commonPricingStageDf1 = sqlContext.sql("""select
 lin_source_system_name__c  
 ,nvl(lin_customer_enterprise_id__c ,"NA") as lin_customer_enterprise_id__c
 ,nvl(lin_workday_cost_center__c ,"NA") as lin_workday_cost_center__c
@@ -259,7 +274,7 @@ lin_source_system_name__c
 ,nvl(fgl ,"NA") as charge_code
 ,nvl(cast(ren_pd as string) ,"NA") as ren_pd
 ,nvl(fbilledby ,"NA") as unit_of_measurement
-,nvl(cast(null as string),"NA") as currencyisocode
+,nvl(currencyisocode,"NA") as currencyisocode
 ,nvl(lin_survivor_customer_name__c ,"NA") as lin_survivor_customer_name__c
 ,nvl(billingstreet ,"NA") as billingstreet
 ,nvl(billingcity ,"NA") as billingcity
@@ -273,7 +288,7 @@ lin_source_system_name__c
 from
 mrs_cust_xref""")
 
-val commonPricingStageDf2 = sqlContext.sql("""select distinct
+val commonPricingStageDf2 = sqlContext.sql("""select
 lin_source_system_name__c
 ,nvl(lin_customer_enterprise_id__c , "NA" ) as lin_customer_enterprise_id__c
 ,nvl(lin_workday_cost_center__c , "NA" ) as lin_workday_cost_center__c
@@ -293,7 +308,7 @@ lin_source_system_name__c
 ,nvl(chargeback_code  , "NA" ) as charge_code
 ,nvl(cast(hj_ren_pd as string) , "NA" ) as ren_pd 
 ,nvl(uom  , "NA" )as unit_of_measurement
-,nvl(cast(null as string),"NA") as currencyisocode
+,nvl(currencyisocode,"NA") as currencyisocode
 ,nvl(lin_survivor_customer_name__c , "NA" ) as lin_survivor_customer_name__c
 ,nvl(billingstreet , "NA" ) as billingstreet
 ,nvl(billingcity , "NA" ) as billingcity
@@ -321,6 +336,31 @@ commonPricingStageDf1.write.mode("append").jdbc(url, "go2", prop)
 //commonPricingStageDF.registerTempTable("common_pricing_stage")
 
 commonPricingStageDf2.write.mode("append").jdbc(url, "go2", prop)
+
+val chargeDimDf = sqlContext.sql("""select distinct
+cast(lin_consolidated_charge_code__c as string) as normalised_charge_code
+,lin_consolidated_charge_name__c as normalised_charge_name
+from 
+antuit_pricing.chargecode_xref""")
+
+chargeDimDf.write.jdbc(url, "charge_dim_test", prop)
+
+val costCenterDimDf = sqlContext.sql("""select distinct
+lin_workday_cost_center__c as cost_center
+,lin_workday_location_id__c as workday_location
+from 
+antuit_pricing.costcenter_xref""")
+
+costCenterDimDf.write.jdbc(url, "cost_center_dim_test", prop)
+
+val customerDimDf = sqlContext.sql("""select distinct
+lin_customer_enterprise_id__c AS enterprise_id
+,lin_survivor_customer_name__c as normalised_client_name
+from
+antuit_pricing.customer_xref""")
+
+customerDimDf.write.jdbc(url, "customer_dim_test", prop)
+
 
 	}
 	
--- Lineage/src/main/scala/com/lineage/db/pricingMart1.scala
+++ Lineage/src/main/scala/com/lineage/db/pricingMart1.scala
@@ -24,20 +24,43 @@ object pricingMart1 {
 					val username = "root"
 					val password = "TiMLRHdCNLftYOLskOkF"
 					
-					val data = sqlContext.sql("select transaction_date,product,price,payment_type,name,city,state,country,account_created,last_login,latitude,longitude from accelos.salessample")
-					data.registerTempTable("salessample_temp1")
+			//		val data = sqlContext.sql("select transaction_date,product,price,payment_type,name,city,state,country,account_created,last_login,latitude,longitude from accelos.salessample")
+			//		data.registerTempTable("salessample_temp1")
 
-					val res1 = sqlContext.sql("select product, count(*) from salessample_temp1 group by product")
+					//val res1 = sqlContext.sql("select product, count(*) from salessample_temp1 group by product")
 
 					val prop = new java.util.Properties
 					prop.setProperty("user",username)
 					prop.setProperty("password",password)
 
-					res1.write.jdbc(url, "test3", prop)
+				//	res1.write.jdbc(url, "test3", prop)
+					
+val chargeDimDf = sqlContext.sql("""select distinct
+cast(lin_consolidated_charge_code__c as string) as normalised_charge_code
+,lin_consolidated_charge_name__c as normalised_charge_name
+from 
+antuit_pricing.chargecode_xref""")
 
+chargeDimDf.write.jdbc(url, "charge_dim_test", prop)
+
+val costCenterDimDf = sqlContext.sql("""select distinct
+lin_workday_cost_center__c as cost_center
+,lin_workday_location_id__c as workday_location
+from 
+antuit_pricing.costcenter_xref""")
+
+costCenterDimDf.write.jdbc(url, "cost_center_dim_test", prop)
+
+val customerDimDf = sqlContext.sql("""select distinct
+lin_customer_enterprise_id__c AS enterprise_id
+,lin_survivor_customer_name__c as normalised_client_name
+from
+antuit_pricing.customer_xref""")
+
+customerDimDf.write.jdbc(url, "customer_dim_test", prop)
 					
 
-					res1.write.mode("append").jdbc(url, "test3", prop)
+					//res1.write.mode("append").jdbc(url, "test3", prop)
 
 
 	}
