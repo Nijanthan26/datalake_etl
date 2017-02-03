6f1f82011ae89e1964204943136ae9892d49af68
SHVinuta <vinuta.hegde@antuit.com>
2017-02-03 12:09:51 +0530

    acl added

--- Lineage/src/main/scala/com/lineage/FirstDump.scala
+++ Lineage/src/main/scala/com/lineage/FirstDump.scala
@@ -13,32 +13,86 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.functions._
 
 object FirstDump {
-  
-  def addDeltaFirstTime(deltaDf: Dataset[Row]): Dataset[Row] = {
-			    val sparkSession = deltaDf.sparkSession
+
+	def addDeltaFirstTime(deltaDf: Dataset[Row]): Dataset[Row] = {
+			val sparkSession = deltaDf.sparkSession
 					val deltaDfSha = RowHash.addHash(deltaDf)
 					val deduped = deltaDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).	map { case(sha2, row) => row }
 					val dedupedDf = sparkSession.createDataFrame(deduped, deltaDfSha.schema) 
-					dedupedDf.createOrReplaceTempView("deduped")
-					import org.apache.spark.sql.functions._ 
-					dedupedDf.withColumn("sequence", monotonically_increasing_id) 
-    
-			    
+							dedupedDf.createOrReplaceTempView("deduped")
+							import org.apache.spark.sql.functions._ 
+							dedupedDf.withColumn("sequence", monotonically_increasing_id) 
+
+
 	}
-    def main(args: Array[String]): Unit = {
-      val conf = new SparkConf().setAppName("FirstDump")
-      val sc = new SparkContext(conf)
-      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-      import sqlContext.implicits._
- 
-      //val archData = sqlContext.sql("select * from archimport."+args(2)) // Load archive data
-      val LatestData = sqlContext.sql("select * from  "+args(1)) // Load latest data from impala
-      val res = addDeltaFirstTime(LatestData)
-      //res.show()
-      res.registerTempTable("mytempTable")
-      sqlContext.sql("drop table if exists antuit_stage."+args(0))
-      sqlContext.sql("create table antuit_stage."+args(0)+" as select * from mytempTable");
-          
-      sqlContext.sql("insert into antuit_stage.dl_t_sequencetrack select CURRENT_TIMESTAMP,\'"+ args(0) +"\',max(sequence) from antuit_stage."+ args(0)) 
-      }
+
+
+
+	def addDeltaFirstTimeAcl(cciDf: Dataset[Row], txDf: Dataset[Row]): Dataset[Row] = {
+			val sparkSession = cciDf.sparkSession
+
+					val cciDfSha = RowHash.addHash(cciDf)
+					val txDfSha = RowHash.addHash(txDf)
+
+					val deduped = cciDfSha.union(txDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).map { case(sha2, row) => row }
+					val dedupedDf = sparkSession.createDataFrame(deduped, cciDfSha.schema) 
+
+							dedupedDf.createOrReplaceTempView("deduped")
+
+							import org.apache.spark.sql.functions._ 
+							dedupedDf.withColumn("sequence", monotonically_increasing_id) 
+
+
+	}
+
+	def main(args: Array[String]): Unit = {
+			val conf = new SparkConf().setAppName("FirstDump")
+					val sc = new SparkContext(conf)
+					val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+					import sqlContext.implicits._
+
+					val antuitStageTablename = args(0)
+					val deltaTable = args(1)
+					
+					sqlContext.sql("insert into antuit_stage.dl_t_sequencetrack select CURRENT_TIMESTAMP,\'"+ antuitStageTablename +"\',max(sequence) from "+ antuitStageTablename)  
+
+					val table = deltaTable.substring(deltaTable.indexOf(".")+1)
+					val db = deltaTable.substring(0,deltaTable.indexOf("."))
+
+
+
+					if(table.startsWith("acl_")){
+
+						val deltaTableCci = "acl_cci_"+table.substring(4)
+						val deltaTableTx = "acl_tx_"+table.substring(4)
+
+						val dfDeltacci = sqlContext.sql("select  tab.*, 'CCI' as source , concat(tab.comp_code,concat('_','CCI'))  as global_compcode from  "+db+"."+deltaTableCci+" tab") //load the Previously Processes table  from Data Lake
+						val dfDeltatx = sqlContext.sql("select  tab.*, 'TX' as source , concat(tab.comp_code,concat('_','TX'))  as global_compcode from  "+db+"."+deltaTableTx+" tab") // Load the delta data from Impala
+
+
+						val res = addDeltaFirstTimeAcl(dfDeltacci,dfDeltatx)
+						res.registerTempTable("mytempTable")
+
+
+						sqlContext.sql("drop table if exists "+antuitStageTablename)
+						sqlContext.sql("create table "+antuitStageTablename+" as select * from mytempTable");
+
+					}
+					else
+					{
+
+						//val archData = sqlContext.sql("select * from archimport."+args(2)) // Load archive data
+						  val LatestData = sqlContext.sql("select * from  "+deltaTable) // Load latest data from impala
+							val res = addDeltaFirstTime(LatestData)
+
+							res.registerTempTable("mytempTable")
+							sqlContext.sql("drop table if exists antuit_stage."+antuitStageTablename)
+							sqlContext.sql("create table antuit_stage."+antuitStageTablename+" as select * from mytempTable");
+						
+
+					}
+
+
+			// sqlContext.sql("insert into antuit_stage.dl_t_sequencetrack select CURRENT_TIMESTAMP,\'"+ args(0) +"\',max(sequence) from antuit_stage."+ args(0)) 
+	}
 }
