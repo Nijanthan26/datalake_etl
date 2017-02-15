8d67b25e16fc0521a2e3ab17ae8ad3f6a6bb9a4f
SHVinuta <vinuta.hegde@antuit.com>
2017-02-15 16:58:09 +0530

    prqt

--- Lineage/src/main/scala/com/lineage/test.scala
+++ Lineage/src/main/scala/com/lineage/test.scala
@@ -9,71 +9,72 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 import scala.reflect.runtime.universe
-import java.util.Calendar
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.functions._
 
 object test {
-
-
-	def addDeltaFirstTimeAcl(cciDf: Dataset[Row], txDf: Dataset[Row]): Dataset[Row] = {
-			val sparkSession = cciDf.sparkSession
-
-					val cciDfSha = RowHash.addHash(cciDf)
-					val txDfSha = RowHash.addHash(txDf)
-
-					val deduped = cciDfSha.union(txDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).map { case(sha2, row) => row }
-					val dedupedDf = sparkSession.createDataFrame(deduped, cciDfSha.schema) 
-
-							dedupedDf.createOrReplaceTempView("deduped")
-
-							import org.apache.spark.sql.functions._ 
-							dedupedDf.withColumn("sequence", monotonically_increasing_id) 
-
-
+/*
+ * To add md5 and sequence numbers to archive data and the latest data present in impala and dump the combined data in data lake.
+ * This is one time run. 
+ * */
+	def addDeltaFirstTimeWithArc(initialDf: Dataset[Row], deltaDf: Dataset[Row]): Dataset[Row] = {
+			    val sparkSession = deltaDf.sparkSession
+            
+             
+          // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
+			    val sortedinitialDf = initialDf.select("archive_date" , deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
+			    val sortedDelta =     deltaDf.select("archive_date" , deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
+          val initialDfSha = RowHash.addHash(sortedinitialDf)//.drop("archive_date"))
+					val deltaDfSha = RowHash.addHash(sortedDelta)//.drop("archive_date"))
+					val deduped = initialDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).	map { case(sha2, row) => row }
+					val dedupedDf = sparkSession.createDataFrame(deduped, deltaDfSha.schema) 
+					dedupedDf.createOrReplaceTempView("deduped")
+					import org.apache.spark.sql.functions._ 
+					dedupedDf.withColumn("sequence", monotonically_increasing_id) 
+    
+			    
 	}
-
-
-	def main(args: Array[String]): Unit = {
-			val conf = new SparkConf().setAppName("DeltaAdd")
-					val sc = new SparkContext(conf)
-					val sqlContext = new org.apache.spark.sql.SQLContext(sc) 
-					import sqlContext.implicits._
-					val antuitStageTablename = args(0)
-					val deltaTable = args(1)
-				//	sqlContext.sql("insert into antuit_stage.dl_t_sequencetrack select CURRENT_TIMESTAMP,\'"+ args(0) +"\',max(sequence) from "+ args(0))  
-
-									val table = deltaTable.substring(deltaTable.indexOf(".")+1)
-									val db = deltaTable.substring(0,deltaTable.indexOf("."))
-
-				
-
-						if(table.startsWith("acl_")){
-
-
-
-									val deltaTableCci = "acl_cci_"+table.substring(4)
-									val deltaTableTx = "acl_tx_"+table.substring(4)
-
-									val dfDeltacci = sqlContext.sql("select  tab.*, 'CCI' as source , concat(tab.comp_code,concat('_','CCI'))  as global_compcode from  "+db+"."+deltaTableCci+" tab") //load the Previously Processes table  from Data Lake
-									val dfDeltatx = sqlContext.sql("select  tab.*, 'TX' as source , concat(tab.comp_code,concat('_','TX'))  as global_compcode from  "+db+"."+deltaTableTx+" tab") // Load the delta data from Impala
-
-
-									val res = addDeltaFirstTimeAcl(dfDeltacci,dfDeltatx)
-									res.registerTempTable("mytempTable")
-
-
-									sqlContext.sql("drop table if exists "+args(0))
-									sqlContext.sql("create table "+args(0)+" as select * from mytempTable");
-
-						}
-						else
-						{
-
-						  println("exitting.......................... " + table)
-
-						}
-			
-			//sc.close()
+	
+		def addDeltaFirstTimeNoArc(deltaDf: Dataset[Row]): Dataset[Row] = {
+			    val sparkSession = deltaDf.sparkSession
+          // val sortedCols = "archive_date" +: deltaDf.columns.filter(x => !x.equals("archive_date"))
+			    val sortedDelta =     deltaDf.select("archive_date" , deltaDf.columns.filter(x => !x.equals("archive_date")):_*)
+         // val initialDfSha = RowHash.addHash(sortedDelta)//.drop("archive_date"))
+					val deltaDfSha = RowHash.addHash(sortedDelta)//.drop("archive_date"))
+					val deduped = deltaDfSha.union(deltaDfSha).rdd.map { row => (row.getString(row.length-1), row) }.reduceByKey((r1, r2) => r1).	map { case(sha2, row) => row }
+					val dedupedDf = sparkSession.createDataFrame(deduped, deltaDfSha.schema) 
+					dedupedDf.createOrReplaceTempView("deduped")
+					import org.apache.spark.sql.functions._ 
+					dedupedDf.withColumn("sequence", monotonically_increasing_id) 
+    
+			    
 	}
+		
+  
+  def main(args: Array[String]): Unit = {
+      val conf = new SparkConf().setAppName("FirstDump")
+      val sc = new SparkContext(conf)
+      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+      import sqlContext.implicits._
+      if(args(2).equals("_"))
+      {
+      //val archData = sqlContext.sql("select * from archimport."+args(2)) // Load archive data
+      val LatestData = sqlContext.sql("select * from sqoopdailydelta."+args(1)) // Load latest data from impala
+      val res = addDeltaFirstTimeNoArc(LatestData)
+      //res.show()
+      res.write.format("parquet").saveAsTable(args(0))
+          }
+      else
+      {
+      val archData = sqlContext.sql("select * from archimport."+args(2)) // Load archive data
+      val LatestData = sqlContext.sql("select * from sqoopdailydelta."+args(1)) // Load latest data from impala
+      val res = addDeltaFirstTimeWithArc(archData, LatestData)
+      //res.show()
+     res.write.format("parquet").saveAsTable(args(0))
+      }
+
+   sqlContext.sql("insert into antuit_stage.dl_t_sequencetrack select CURRENT_TIMESTAMP,\'"+ args(0) +"\',max(sequence) from antuit_stage."+ args(0))  
+  
+      }
+  
 }
