aa3cdd926f4b3684251340a74000b54ccbe57abe
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-17 15:59:01 +0530

    mrs

---
+++ Lineage/src/main/scala/com/lineage/mrs.scala
@@ -0,0 +1,30 @@
+package com.lineage
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import org.apache.spark.sql.SQLContext
+import com.microsoft.sqlserver.jdbc.SQLServerDriver
+
+object mrs {
+  
+  def main(args: Array[String]) {
+    
+    val conf = new SparkConf().setAppName("MRS Import")
+    val sc = new SparkContext(conf)
+    val tablename = args [0]
+    
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    
+    val tableData = sqlContext.read.format("jdbc")
+    option("url", "jdbc:sqlserver://us0266sqlsrvmrs001.database.windows.net:1433;databaseName=US0009SQLDBFacilityData09_001").
+    option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver").
+    option("dbtable", tablename).
+    option("user", "readonly").
+    option("password", "R3@60n1Y$").load()
+    
+    
+  }
+  
+}
