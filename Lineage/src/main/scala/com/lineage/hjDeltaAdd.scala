564a384f91f8f1923c1f0d430ce797ae93f9d14b
DarshanaGautam <darshana.gautam@antuit.com>
2017-02-20 13:40:09 +0530

    added string

--- Lineage/src/main/scala/com/lineage/hjDeltaAdd.scala
+++ Lineage/src/main/scala/com/lineage/hjDeltaAdd.scala
@@ -13,7 +13,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.functions._
 object hjDeltaAdd {
 
-	def addDeltaIncremental(initialDfShaWithDate: DStream[String], deltaDf: DStream[String]): DStream[String] = {
+	def addDeltaIncremental(initialDfShaWithDate: String, deltaDf: String): String = {
 			val initialDfSha = initialDfShaWithDate//.drop("archive_date")
 					val sparkSession = deltaDf.sparkSession
 					val  delta = deltaDf
